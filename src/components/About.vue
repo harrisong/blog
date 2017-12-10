ed494c306bf05a5e4c624c93203f33830fde9633
Harrison N <tplawyer@gmail.com>
2017-12-09 19:16:15 -0800

    vue landing

---
+++ src/components/About.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <img id="avatar" src="../assets/avatar.jpg">
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for='link in links'><a :href='link.href'>{{ link.name }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      title: 'Entomologist specialised in code',
+      links: [
+        {
+          name: 'Github',
+          href: 'http://github.com/harrisong'
+        },
+        {
+          name: 'Linkedin',
+          href: 'https://www.linkedin.com/in/harrison-ng-85958542/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#avatar {
+  width: 200px;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <span>{{ descriptions }}</span>
+    <ul>
+      <li><a href="/about">About</a></li>
+      <li><a href="/blog">Blog</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Harrison Ng',
+      descriptions: 'Some recent updates'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+  & + span {
+
+  }
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

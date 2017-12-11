ea4ea5053d4514f294ec696cb987ed7478bd4f34
Harrison N <tplawyer@gmail.com>
2017-12-10 19:56:10 -0800

    basic static

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -3,8 +3,16 @@
     <router-link id="back" to="/">Back</router-link>
     <h1>{{title}}</h1>
     <span>{{descriptions}}</span>
-    <ul>
-      <li v-for='link in links'></li>
+    <ul class="container">
+      <li class="post" v-for='post in posts'>
+        <div class="postMeta">
+          <h3><router-link v-bind:to="'/blog/' + post.id">{{post.title}}</router-link></h3>
+          <div class="date">{{post.date}}</div>
+        </div>
+        <div>
+          {{post.shortDescriptions}}
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -18,7 +26,10 @@ export default {
       descriptions: 'I hightly doubt I will post any updates here 🌬️',
       posts: [
         {
-          title: ''
+          id: 0,
+          title: 'First post',
+          date: '2017-12-10',
+          shortDescriptions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nunc elit, egestas ut lacus feugiat, vehicula ultricies ligula. Integer dictum luctus orci, quis euismod nisl maximus quis. In a velit lobortis, laoreet massa rutrum, tempor ex. Proin fermentum elementum dolor nec bibendum. Donec eget sem quam. Integer imperdiet ex eu enim dapibus, vitae commodo tortor dapibus. Cras dapibus ullamcorper bibendum. Suspendisse dapibus libero id nunc congue pretium.'
         }
       ]
     }
@@ -28,4 +39,23 @@ export default {
 
 <style lang="scss" scoped>
 @import "../scss/global.scss";
+.post {
+  grid-column-start: 2;
+
+  .postMeta {
+    display: flex;
+    margin: 1.5rem 0;
+    h3, .date {
+      width: 50%;
+      margin: 0;
+      line-height: 1rem;
+    }
+    .date {
+      font-style: italic;
+      vertical-align: baseline;
+      color: $light-blue;
+    }
+  }
+
+}
 </style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-link id="back" to="/">Back</router-link>
+    <router-link id="back" to="/blog">Back</router-link>
     <h1>{{title}}</h1>
     <span>{{descriptions}}</span>
     <div>
@@ -23,31 +23,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-h1, h2 {
-  font-weight: normal;
-  + span {
-    font-style: italic;
-  }
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #8F616E;
-}
-#back {
-  position: absolute;
-  top: 30px;
-  left: 30px;
-}
-#avatar {
-  width: 200px;
-  border-radius: 5px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
-}
+@import "../scss/global.scss";
 </style>
